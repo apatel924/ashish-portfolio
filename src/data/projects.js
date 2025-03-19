d0f633a663540fe554803a6fc9382ae2e8c6924d
Ashish Patel <apatel2@ualberta.ca>
2025-03-18 23:17:39 -0600

    starter array for projects

---
+++ src/data/projects.js
@@ -0,0 +1,18 @@
+export const projectsData = [
+  {
+    title: "Quiz Arena",
+    description:
+      "An interactive quiz platform designed to provide an engaging user experience...",
+    technologies: [
+      "React.js",
+      "React Native",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Socket.io",
+      "Expo",
+    ],
+    thumbnail: "/images/quiz-arena.png",
+    link: "https://quizarena.app",
+  },
+];
