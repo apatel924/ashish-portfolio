cbd25d196f4c68af5e259ac19788d439cf3e4945
Ashish Patel <apatel2@ualberta.ca>
2025-03-18 23:16:15 -0600

    added starter code for expereinces

---
+++ src/data/experiences.js
@@ -0,0 +1,9 @@
+export const experienceData = [
+  {
+    period: "2023-12 — PRESENT",
+    title: "Full Stack Developer / iOS Developer",
+    company: "Quiz Arena",
+    description: "Co-founded and developed the Quiz Arena app...",
+    technologies: ["React Native", "Node.js", "MongoDB", "Socket.io", "Expo"],
+  },
+];
