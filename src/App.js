86bacc96b792268c30ada7eaf1c54ade84083338
Ashish Patel <apatel2@ualberta.ca>
2025-03-18 23:09:14 -0600

    barebones setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1> initial setuep</h1>
+      <p>Area for portfolio code</p>
+    </div>
+  );
 }
 
 export default App;
