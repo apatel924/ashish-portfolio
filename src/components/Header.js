ffb45f8cc314429ab27e2234b0b33c39e84a730c
Ashish Patel <apatel2@ualberta.ca>
2025-03-18 23:33:16 -0600

    added header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,8 @@ const Header = () => {
   return (
     <header style={containerStyle}>
       <h1 style={nameStyle}>Ashish Patel</h1>
+      <h2 style={titleStyle}></h2>
+      <p style={paragraphStyle}>my portfiolo</p>
     </header>
   );
 };
