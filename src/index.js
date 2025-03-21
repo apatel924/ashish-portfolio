2ac227784c405faf914ab48e16c4eb46c9d1f86f
Ashish Patel <apatel2@ualberta.ca>
2025-03-21 01:03:28 -0600

    added header

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ backgroundColor: "lightblue", height: "100vh" }}>
-      <h1 style={{ textAlign: "center" }}>Hello World</h1>
-    </div>
-  );
-}
+import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
