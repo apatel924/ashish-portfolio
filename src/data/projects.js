d1273c4ed1622b19480176620462cef5c34ec43f
Ashish Patel <apatel2@ualberta.ca>
2025-03-21 01:14:27 -0600

    added projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -15,4 +15,41 @@ export const projectsData = [
     thumbnail: "/images/quiz-arena.png",
     link: "https://quizarena.app",
   },
+  {
+    title: "Remote-Together",
+    description:
+      "Platform with the ability to find specific remote working locations...",
+    technologies: [
+      "React.js",
+      "Google Maps API",
+      "Material UI",
+      "Tailwind CSS",
+      "Node.js",
+      "Express",
+      "PostgreSQL",
+      "Socket.io",
+    ],
+    thumbnail: "/images/remote-together.png",
+  },
+  {
+    title: "Resource-Wall",
+    description:
+      "A Pinterest-like app with the ability to create new resource links...",
+    technologies: ["Node.js", "PostgreSQL", "EJS", "Express", "SASS"],
+    thumbnail: "/images/resource-wall.png",
+  },
+  {
+    title: "Interview Scheduler",
+    description:
+      "A React app that works as an appointment scheduler with multiple testing tools...",
+    technologies: [
+      "React",
+      "Webpack",
+      "Axios",
+      "Storybook",
+      "Jest",
+      "Testing Library",
+    ],
+    thumbnail: "/images/interview-scheduler.png",
+  },
 ];
