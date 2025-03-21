8051aed0f1b8ed60e828c60fe9cc5f74f959601c
Ashish Patel <apatel2@ualberta.ca>
2025-03-21 01:03:09 -0600

    changes to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
 import React from "react";
 
 const Header = () => {
-  const containerStyle = {
-    marginBottom: 40,
+  const headerStyle = {
+    backgroundColor: "#0a192f",
+    color: "#e6f1ff",
+    padding: "20px",
+    textAlign: "center",
   };
 
-  const nameStyle = {};
-
-  const titleStyle = {};
-
-  const paragraphStyle = {};
-
   return (
-    <header style={containerStyle}>
-      <h1 style={nameStyle}>Ashish Patel</h1>
-      <h2 style={titleStyle}></h2>
-      <p style={paragraphStyle}>my portfiolo</p>
+    <header style={headerStyle}>
+      <h1>Ashish Patel</h1>
+      <h2>Full Stack Web Developer</h2>
     </header>
   );
 };
+
+export default Header;
