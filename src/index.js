f3c2c89ac3589912af2bffedbfa4f95e014f2f58
Ashish Patel <apatel2@ualberta.ca>
2025-03-20 22:35:24 -0600

    small change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "lightblue", height: "100vh" }}>
+      <h1 style={{ textAlign: "center" }}>Hello World</h1>
+    </div>
+  );
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-
 root.render(<App />);
