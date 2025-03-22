5cab5cbba9b9451bb0bff99d3e2ef9668833413e
Ashish Patel <apatel2@ualberta.ca>
2025-03-22 02:52:04 -0600

    Modified LeftSidebar.js to enhance user experience and fix layout issues

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -1,78 +1,82 @@
+// src/components/LeftSidebar.js
 import React from "react";
 
-const LeftSidebar = () => {
+function LeftSidebar() {
+  // Full-height column
   const containerStyle = {
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center",
-    padding: "20px",
-    height: "100%", // fill sidebar
+    justifyContent: "space-between", // name/title at top, social links at bottom
+    alignItems: "center",
+    height: "100vh",
     boxSizing: "border-box",
+    padding: "60px",
+    textAlign: "center",
   };
 
+  // Big name
   const nameStyle = {
-    fontSize: "2rem",
-    marginBottom: "10px",
+    fontSize: "4rem",
+    margin: 0,
   };
 
+  // Big title
   const titleStyle = {
-    fontSize: "1.2rem",
+    fontSize: "2rem",
     color: "#64ffda",
-    marginBottom: "20px",
+    margin: "10px 0 30px",
   };
 
+  // Table of contents
   const navStyle = {
-    marginTop: "40px",
-  };
-
-  const ulStyle = {
-    listStyle: "none",
-    padding: 0,
-  };
-
-  const liStyle = {
-    marginBottom: "10px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "20px",
   };
 
   const linkStyle = {
     color: "#e6f1ff",
     textDecoration: "none",
-    fontSize: "14px",
+    fontSize: "1.2rem",
+  };
+
+  const topSectionStyle = {
+    // Container for name, title, nav
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+
+  // Social links at bottom
+  const socialLinksStyle = {
+    marginBottom: "20px",
   };
 
   return (
     <div style={containerStyle}>
-      {/* name and title */}
-      <div>
+      <div style={topSectionStyle}>
         <h1 style={nameStyle}>Ashish Patel</h1>
         <h2 style={titleStyle}>Full Stack Web Developer</h2>
-        <p style={{ color: "#a8b2d1", maxWidth: "200px", lineHeight: 1.4 }}>
-          I build accessible, pixel-perfect digital experiences for the web.
-        </p>
+
+        <nav style={navStyle}>
+          <a href="#about" style={linkStyle}>
+            About Me
+          </a>
+          <a href="#experience" style={linkStyle}>
+            Experience
+          </a>
+          <a href="#projects" style={linkStyle}>
+            Projects
+          </a>
+        </nav>
       </div>
 
-      {/* Navigation */}
-      <div style={navStyle}>
-        <ul style={ulStyle}>
-          <li style={liStyle}>
-            <a href="#about" style={linkStyle}>
-              About
-            </a>
-          </li>
-          <li style={liStyle}>
-            <a href="#experience" style={linkStyle}>
-              Experience
-            </a>
-          </li>
-          <li style={liStyle}>
-            <a href="#projects" style={linkStyle}>
-              Projects
-            </a>
-          </li>
-        </ul>
+      <div style={socialLinksStyle}>
+        {/* Example social links (GitHub, LinkedIn, etc.) */}
+        <p style={{ color: "#8892b0" }}>Social Links Here</p>
       </div>
     </div>
   );
-};
+}
 
 export default LeftSidebar;
