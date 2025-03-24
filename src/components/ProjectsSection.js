654006fb2a99c8b86a913e58e403afb1526299aa
Ashish Patel <apatel2@ualberta.ca>
2025-03-24 09:03:20 -0600

    Enhance ProjectsSection with modal functionality

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
 import { projectsData } from "../data/projects";
+import ProjectModal from "./ProjectModal";
 
 function ProjectsSection() {
+  const [selectedProject, setSelectedProject] = useState(null);
+
   const containerStyle = {
     marginBottom: "100px",
     width: "100%",
     maxWidth: "1200px",
   };
 
-  // const headingStyle = {
-  //   fontSize: "3rem",
-  //   fontWeight: "bold",
-  //   marginBottom: "20px",
-  //   color: "#ffffff",
-  // };
+  // Open the modal with the clicked project
+  const handleOpenModal = (project) => {
+    setSelectedProject(project);
+  };
+
+  // Close the modal
+  const handleCloseModal = () => {
+    setSelectedProject(null);
+  };
 
   return (
     <div id="projects" style={containerStyle}>
-      {/* <h2 style={headingStyle}>Projects</h2> */}
       {projectsData.map((project, index) => (
-        <ProjectCard key={index} {...project} />
+        <ProjectCard
+          key={index}
+          {...project}
+          onCardClick={() => handleOpenModal(project)}
+        />
       ))}
+
+      {/* Modal for the selected project */}
+      <ProjectModal project={selectedProject} onClose={handleCloseModal} />
     </div>
   );
 }
