00cee0eeafaf2a7e27f53e2a557239d1464dbf74
Ashish Patel <apatel2@ualberta.ca>
2025-03-24 10:20:36 -0600

    Update project descriptions and tech stack

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,6 +11,9 @@ import QaIMG9189 from "../images/Qa-Images/IMG_9189.PNG";
 import QaIMG9190 from "../images/Qa-Images/IMG_9190.PNG";
 import QaIMG9191 from "../images/Qa-Images/IMG_9191.PNG";
 import QaIMG9192 from "../images/Qa-Images/IMG_9192.PNG";
+import QaIMG9193 from "../images/Qa-Images/IMG_9193.PNG";
+import QaIMG9194 from "../images/Qa-Images/IMG_9194.PNG";
+import QaIMG9195 from "../images/Qa-Images/IMG_9195.PNG";
 
 import Remote1 from "../images/Remote-together-Images/Remote-together-1.png";
 import Remote2 from "../images/Remote-together-Images/Remote-together-2.png";
@@ -31,25 +34,18 @@ import IS4 from "../images/Interview-scheduler-Images/scheduler-main-page.png";
 export const projectsData = [
   {
     title: "Quiz Arena",
-    description: "Quiz Arena is an interactive quiz platform...",
+    description:
+      "An interactive quiz platform designed to provide an engaging user experience. Led the complete redesign and functionality implementation for the solo mode, developed a customizable profile page with React components, and optimized the backend system for efficient image rendering.",
     technologies: [
       "React.js",
       "React Native",
       "Node.js",
       "Express",
       "MongoDB",
-      "PostgreSQL",
       "Socket.io",
       "Expo",
-      "Storybook",
-      "Jest",
-      "Sentry",
-      "Tailwind CSS",
-      "Material UI",
-      "Apple Development",
     ],
     link: "https://linktr.ee/quizarena",
-    // Use your imported images
     thumbnail: QaDiscover,
     images: [
       QaDiscover,
@@ -64,17 +60,20 @@ export const projectsData = [
       QaIMG9190,
       QaIMG9191,
       QaIMG9192,
+      QaIMG9193,
+      QaIMG9194,
+      QaIMG9195,
     ],
   },
   {
     title: "Remote-Together",
-    description: "Remote-Together is a platform that helps...",
+    description:
+      "Platform with the ability to find specific remote working locations for workers/students. Able to interact with a built in chat that works like a forum, user login, a map that renders markers with locations of interests and business details dynamically.",
     technologies: [
       "Google Maps API",
       "React.js",
       "Material UI",
       "Tailwind CSS",
-      "Axios",
       "Node.js",
       "Express",
       "PostgreSQL",
@@ -86,7 +85,8 @@ export const projectsData = [
   },
   {
     title: "Resource-Wall",
-    description: "Resource-Wall is a Pinterest-inspired platform...",
+    description:
+      "A Pinterest reconstruct with the ability to create new resource links with details and description, ability to like and favorite as well as favorites page, editing page and new resource page with a home page that has the ability to search through all resources.",
     technologies: [
       "Node.js",
       "PostgreSQL",
@@ -105,7 +105,8 @@ export const projectsData = [
   },
   {
     title: "Interview Scheduler",
-    description: "Interview Scheduler is a robust React application...",
+    description:
+      "A React app that works as an appointment scheduler with the ability to edit, add and delete appointments on multiple days of the week with multiple testing implemented such as storybook, jest and cypress.",
     technologies: [
       "React",
       "Webpack",
