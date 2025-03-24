83d8e373a7d0c03517be4893568a29b317d557fa
Ashish Patel <apatel2@ualberta.ca>
2025-03-24 09:03:55 -0600

    added imports

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,33 @@
+// Quiz Arena images
+import QaDiscover from "../images/Qa-Images/Discover-page.png";
+import QaLeaderboard from "../images/Qa-Images/Flat-app-preview-1.png";
+import QaProfile from "../images/Qa-Images/Flat-app-preview.png";
+import QaQuiz from "../images/Qa-Images/Leaderboard-page.png";
+import QaSettings from "../images/Qa-Images/Play-page.png";
+import QaSolo from "../images/Qa-Images/You-lost-page.png";
+import QaIMG9187 from "../images/Qa-Images/IMG_9187.PNG";
+import QaIMG9188 from "../images/Qa-Images/IMG_9188.PNG";
+import QaIMG9189 from "../images/Qa-Images/IMG_9189.PNG";
+import QaIMG9190 from "../images/Qa-Images/IMG_9190.PNG";
+import QaIMG9191 from "../images/Qa-Images/IMG_9191.PNG";
+import QaIMG9192 from "../images/Qa-Images/IMG_9192.PNG";
+
+import Remote1 from "../images/Remote-together-Images/Remote-together-1.png";
+import Remote2 from "../images/Remote-together-Images/Remote-together-2.png";
+import Remote3 from "../images/Remote-together-Images/Remote-together-3.png";
+import Remote4 from "../images/Remote-together-Images/Remote-together-4.png";
+
+import RW1 from "../images/Resource-Wall-Images/Resource-Wall-1.png";
+import RW2 from "../images/Resource-Wall-Images/Resource-Wall-2.png";
+import RW3 from "../images/Resource-Wall-Images/Resource-Wall-3.png";
+import RW4 from "../images/Resource-Wall-Images/Resource-Wall-4.png";
+
+// Interview Scheduler images
+import IS1 from "../images/Interview-scheduler-Images/Scheduler-add-appt.png";
+import IS2 from "../images/Interview-scheduler-Images/Scheduler-added-appt.png";
+import IS3 from "../images/Interview-scheduler-Images/scheduler-delete-appt.png";
+import IS4 from "../images/Interview-scheduler-Images/scheduler-main-page.png";
+
 export const projectsData = [
   {
     title: "Quiz Arena",
